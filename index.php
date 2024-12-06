d55f0c6cb90ec184788ececa0d2fa76ffee70452
Honapona_ <156892939+Honapon@users.noreply.github.com>
2024-12-06 10:53:23 +0100

    adada

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+
+function dbConnect()
+{
+    $dsn = "mysql:host=localhost;dbname=gpu_compare";
+    $username = "gpu";
+    $password = "compare";
+
+  
+
+    try {
+        $connect = new PDO($dsn, $username, $password);
+        $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //echo ("connected to this (yipi)");
+        return $connect;
+    } catch (PDOException $e) {
+        echo 'Connection no work :( </br>' . $e->getMessage();
+        exit;
+    }
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
