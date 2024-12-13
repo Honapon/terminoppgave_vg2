9cb03e6f7f86e3e0eb4fc6a7031eb2a5d69e0344
Honapona_ <156892939+Honapon@users.noreply.github.com>
2024-12-13 10:21:14 +0100

    ø

--- index.php
+++ index.php
@@ -7,8 +7,7 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=grafikkort", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  
-} catch(PDOException $e) {
+} catch (PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
 ?>
@@ -16,26 +15,29 @@ try {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-<div class="navbar" >
-  <a  href="index.php"><img src="logo.png" alt="logo, pixelert grafikkort"></a>
-<h1>aaa</h1>
-<a class="linkknappting" href="brukerstotte.html">forklaringer</a>
-</div>
+  <div class="navbar">
+    <a href="index.php"><img src="logo.png" alt="logo, pixelert grafikkort"></a>
+    <h1>aaa</h1>
+    <a class="linkknappting" href="brukerstotte.html">forklaringer</a>
+  </div>
 
-<div class="content" >
+  <div class="content">
 
-</div>
+  </div>
 
-<div class="footer" >
+  <div class="footer">
 
-</div>
-    
+  </div>
+
 </body>
+
 </html>
