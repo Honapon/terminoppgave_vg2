639c28867cfaa1ef4f41810561f0ab934fd28310
Honapona_ <156892939+Honapon@users.noreply.github.com>
2024-12-19 14:20:10 +0100

    Update tst.php

--- tst.php
+++ tst.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
 <?php
 $servername = "localhost";
 $username = "grafikk";
@@ -27,3 +37,5 @@ if ($result->num_rows > 0) {
 echo "</table>";
 $conn->close();
 ?>
+</body>
+</html>
